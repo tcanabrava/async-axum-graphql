03a2dbed0bd12cca09adcc550a713540af44f87e
Tomaz Canabrava <tcanabrava@kde.org>
2024-04-23 13:59:35 +0200

    First transaction working / adding members

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,80 +1,54 @@
 use axum::{
-    extract::{Path, Json},
+    extract::{Path, Json, State},
     routing::{get,post},
     Router,
+    response::{Html, IntoResponse},
+    http::{StatusCode, Uri, header::{self, HeaderMap, HeaderName}},
 };
 use dotenv;
-
+use std::collections::HashMap;
 use sea_orm::{Database, DatabaseConnection, ConnectOptions};
-use sqlx::database;
 
 use std::time::Duration;
 use std::sync::Arc;
+use serde::Serialize;
+use entity::{prelude::*, *};
 
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Ingredient {
-    name: String,
-    description: String
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Member {
-    name: String,
-    avatar: String,
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Vote {
-    up: u64,
-    down: u64
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct RecipeStep {
-    name: String,
-    description: String,
-    time: Duration,
-    votes: Vote,
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Recipe {
-    name: String,
-    decription: String,
-    steps: Vec<RecipeStep>,
-    votes: Vote,
-}
-struct AppState {
+pub struct AppState {
     db: DatabaseConnection
 }
 
+#[derive(Serialize)]
+pub struct DatabaseInsertion {
+    status: u16,
+    text: String
+}
+
 pub mod v1 {
+    use sea_orm::{ActiveModelTrait, ActiveValue};
+
     use super::*;
 
     pub async fn ok() -> &'static str {
         "Ok\n"
     }
 
-    pub async fn get_ingredient(Path(ingredient_id): Path<String>) {
-    }
+    pub async fn add_member(
+        State(state): State<Arc<AppState>>,
+        Json(payload): Json<entity::member::Model>)
+        -> Json<DatabaseInsertion>
+    {
+        let mut model = member::ActiveModel::from(payload);
+        model.id = ActiveValue::not_set();
 
-    pub async fn post_ingredient(Json(payload): Json<Ingredient>) {
-    }
+        let res = model.insert(&state.db).await;
 
-    pub async fn get_recipe(Path(recipe_id): Path<String>) {
-    }
-
-    pub async fn post_recipe(Json(payload): Json<Recipe>) {
-    }
-
-    pub async fn get_member(Path(member_id): Path<String>) {
-    }
-
-    pub async fn add_member(Json(payload): Json<Member>) {
+        match res {
+            Ok(_) => Json(DatabaseInsertion{status: StatusCode::CREATED.as_u16(), text: "".to_owned()}),
+            Err(err) => Json(DatabaseInsertion{status: StatusCode::CONFLICT.as_u16(), text: err.to_string()})
+        }
     }
 }
-
-
 
 #[tokio::main]
 async fn main() {
@@ -102,8 +76,7 @@ async fn main() {
 
     let app = Router::new()
         .route("/v1/ok", get(v1::ok))
-        .route("/v1/ingredients", post(v1::post_ingredient))
-        .route("/v1/ingredients/:ingredient_id", get(v1::get_ingredient))
+        .route("/v1/member", post(v1::add_member))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
