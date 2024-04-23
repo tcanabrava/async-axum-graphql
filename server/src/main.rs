5573705323f90c6b9f141d84cdeae0dd5b0bf819
Tomaz Canabrava <tcanabrava@kde.org>
2024-04-23 12:52:30 +0200

    Code clarity

--- server/src/main.rs
+++ server/src/main.rs
@@ -78,12 +78,13 @@ pub mod v1 {
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt()
-    .with_max_level(tracing::Level::DEBUG)
-    .with_test_writer()
-    .init();
-
     dotenv::dotenv().ok();
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
+
     let database_url = dotenv::var("DATABASE_URL").unwrap();
     let mut opt = ConnectOptions::new(database_url);
     
